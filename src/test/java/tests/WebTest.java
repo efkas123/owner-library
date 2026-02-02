8aab04446354d80398a48a37ee3918eca423272d
Filipp Kotov <contact@filippkotov.ru>
2026-02-02 11:58:28 +0300

    Created WebTest and WebTestBase classes

--- src/test/java/tests/WebTest.java
+++ src/test/java/tests/WebTest.java
@@ -1,5 +1,16 @@
 package tests;
 
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class WebTest {
 
+    @Test
+    public void BSTest1(){
+        open("/");
+        $("span[data-target='qbsearch-input.inputButtonText']").shouldHave(text("Search or jump to..."));
+    }
 }
--- src/test/java/tests/WebTestBase.java
+++ src/test/java/tests/WebTestBase.java
@@ -1,4 +1,34 @@
 package tests;
 
+
+import config.ConfigProvider;
+import config.WebConfig;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.Configuration.*;
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
 public class WebTestBase {
+
+    private static final WebConfig config = ConfigProvider.webConfig();
+
+
+    @BeforeAll
+    static void SetUp() {
+        browser = config.browser();
+        baseUrl = config.baseUrl();
+        browserVersion = config.browserVersion();
+        pageLoadStrategy = "eager";
+
+        if (config.isRemote()) {
+            remote = config.remoteUrl();
+        }
+    }
+
+    @AfterAll
+    static void TearDown() {
+        closeWebDriver();
+    }
+
 }
